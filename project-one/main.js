6ae2a99cbcd115ffae2920ceb264d749c01b14ee
jihadrekani <jihad.honest@gmail.com>
2022-06-26 14:15:32 +0300

    changed button to a

--- project-one/main.js
+++ project-one/main.js
@@ -4,15 +4,22 @@ function resize() {
     let url = document.getElementById('url').value;
     let height = document.getElementById('height').value;
     let width = document.getElementById('width').value;
+    
   
     /* only returns index number of last occurence of the forward slash : 27 */
     let lastIndex = url.lastIndexOf('/');
-    //
+    
     let lastPart = url.substring(lastIndex);
-    let generatedUrl = `https://source.unsplash.com${lastPart}/${width}x${height}`;
+     let generatedUrl = `https://source.unsplash.com${lastPart}/${width}x${height}`;
   
-    alert(generatedUrl);
+     alert(generatedUrl);
   
     // https://unsplash.com/photos/mASiL-TP0eU
     // https://source.unsplash.com/mASiL-TP0eU/heightxwidth
-  }
+   }
+
+
+   
+
+
+ 
