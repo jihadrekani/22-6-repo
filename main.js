6e4d6f537a65003cc5e8e2ed3047089381788552
jihadrekani <jihad.honest@gmail.com>
2022-06-30 14:54:43 +0300

    changed skills section shape

--- main.js
+++ main.js
@@ -6,3 +6,66 @@ const body = document.getElementById("body")
 dark.addEventListener("click" , ()=>{
     body.classList.toggle("dark")
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// select skills selector
+let myskills = document.querySelector(".skills");
+
+window.onscroll = function (){
+
+    //skills offset top
+    let skillsOffsetTop = myskills.offsetTop;
+
+    // skills outer height
+    let skillsOuterHeight = myskills.offsetHeight;
+
+    //window height 
+    let windowHeight = this.innerHeight;
+
+    //window scrollTop
+    let windowScrollTop = this.pageYOffset;
+
+
+    if (windowScrollTop > (skillsOffsetTop + skillsOuterHeight - windowHeight)) {
+
+        let allSkills = document.querySelectorAll(".skill-box .skill-progress span")
+
+    allSkills.forEach(skill => {
+        skill.style.width = skill.dataset.progress;
+    })
+        
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
